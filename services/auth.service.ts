3745f935d2794dd000ca20aaedf502481029e475
Pierre <bonnamy.77@gmail.com>
2022-07-03 23:48:15 +0200

    fix login / register

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,5 +1,5 @@
-import {AuthUtil} from "../utils/auth.util";
-import {UserDocument, UserModel, UserProps} from "../models/user.model";
+import {AuthUtil} from "../utils";
+import {UserDocument, UserModel, UserProps} from "../models";
 
 const jwt = require('jsonwebtoken')
 
@@ -16,59 +16,37 @@ export class AuthService {
 
     private constructor() { }
 
-    public async register(props: Partial<UserProps>): Promise<UserDocument> {
+    async getOneByLogin(login: string): Promise<UserDocument | null> {
+        return await UserModel.findOne({login : login}).exec();
+    }
 
-        if(!props.password) {
-            console.log(props.password);
-            throw new Error('Missing parameters test');
-        }
+    public async register(props: Partial<UserProps>): Promise<UserDocument> {
 
         const model = new UserModel({
             login: props.login,
-            password: AuthUtil.sha512(props.password),
+            password: AuthUtil.sha512(props.password!),
             type: props.type,
-            name: props.name,
-            address: props.address,
-            phone: props.phone
         });
 
-        const user = await model.save();
-        await user.populate("type");
-
-        return user;
+        return await model.save();
     }
 
     public async logIn(props: Partial<UserProps>): Promise<string> {
-        if(!props.password) {
-            throw new Error('Missing parameters');
-        }
 
         const user = await UserModel.findOne({
             login: props.login,
-            password: AuthUtil.sha512(props.password),
+            password: AuthUtil.sha512(props.password!),
         }).exec();
 
         if (!user) {
             throw new Error('User not found');
         }
 
-        return jwt.sign({
+        return await jwt.sign({
             id: user.id,
-            username: user.login,
+            email: user.login,
             type: user.type
         }, process.env.SECRET, {expiresIn: '1d'})
-
-        /*const token = jwt.sign({
-            id: user.id,
-            username: user.login,
-            type: user.type
-        }, process.env.SECRET, {expiresIn: '1d'})
-
-        const extracted = AuthUtil.getToken(token)
-
-        const content = jwt.decode(extracted, {complete: false})
-
-        return content;*/
     }
 
     public me(token: string | undefined): string {
